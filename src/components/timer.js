2a0dbd6d07927862e539ce4853f21fdb17b82c72
RyanBlakeFair <ryanblakefair@gmail.com>
2020-10-26 15:32:21 +1300

    Update timer.js

--- src/components/timer.js
+++ src/components/timer.js
@@ -84,7 +84,7 @@ function Timer(props) {
           className="text-4xl text-white text-center font-bold"
           style={{ textTransform: "uppercase" }}
         >
-          Hello {props.displayName}
+          Hi, {props.displayName}
         </p>
         <div className="timerdiv ml-auto mr-auto rounded pb-12 pt-8">
           <p
